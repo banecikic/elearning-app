fee5b4008271721efc76a66c5a45a30fa4749207
banecikic <branimir.cikic@gmail.com>
2021-02-19 14:01:05 +0100

    home page style improvement

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
 
   def index
     @courses = Course.all.limit(3)
-    @latest_couses = Course.all.limit(3).order(created_at: :desc)
+    @latest_courses = Course.all.limit(3).order(created_at: :desc)
   end
 end
