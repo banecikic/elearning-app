b568823c518ab2a0c0b11c06cf3b0cdd128574ca
banecikic <branimir.cikic@gmail.com>
2021-02-25 14:36:14 +0100

    update email sender in devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'support@elearning.herokuapp.com'
+  config.mailer_sender = 'branimir.cikic@gmail.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
