adbb3e5d7462ed705fd095073595b9ff998d664d
banecikic <branimir.cikic@gmail.com>
2021-02-26 12:26:06 +0100

    fix confirmable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { :host => 'elearning-app.com', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => 'elearning-rails-app.herokuapp.com', :protocol => 'https' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { confirmations: 'confirmations' }
+  devise_for :users
   resources :courses
   resources :users, only: [:index]
   root "home#index"
