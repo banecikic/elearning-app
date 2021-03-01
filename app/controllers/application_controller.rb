a3d9902a3749140e1f51dd3b61c81397f34faa90
banecikic <branimir.cikic@gmail.com>
2021-03-01 13:54:47 +0100

    add footer etc...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  include PublicActivity::StoreController 
 
   before_action :set_global_variables, if: :user_signed_in?
   def set_global_variables
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :courses
   resources :users, only: [:index]
+  get "home/activity"
   root "home#index"
   get 'privacy_policy', to: "static_pages#privacy_policy"
-
 end
