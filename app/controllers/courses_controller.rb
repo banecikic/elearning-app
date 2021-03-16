b900dc3a2300a600e7742920e638b0c81e3807c6
banecikic <branimir.cikic@gmail.com>
2021-03-16 12:20:34 +0100

    enrolments stuff

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,17 +2,32 @@ class CoursesController < ApplicationController
   before_action :set_course, only: %i[ show edit update destroy ]
 
   def index
-    # if params[:title]
-    #   @courses = Course.where('title ILIKE ?', "%#{params[:title]}%")
-    # else
-    #   #@courses = Course.all
-    #   @q = Course.ransack(params[:q])
-    #   @courses = @q.result.includes(:user)
-    # end
+    @ransack_path = courses_path
+
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
-    # @courses = @ransack_courses.result.includes(:user)
 
     @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
+  end
+
+  def purchased
+    @ransack_path = purchased_courses_path
+    @ransack_courses = Course.joins(:enrollments).where(enrollments: {user: current_user}).ransack(params[:courses_search], search_key: :courses_search)
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
+    render 'index'
+  end
+
+  def pending_review
+    @ransack_path = pending_review_courses_path
+    @ransack_courses = Course.joins(:enrollments).merge(Enrollment.pending_review.where(user: current_user)).ransack(params[:courses_search], search_key: :courses_search)
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
+    render 'index'
+  end
+
+  def created
+    @ransack_path = created_courses_path
+    @ransack_courses = Course.where(user: current_user).ransack(params[:courses_search], search_key: :courses_search)
+    @pagy, @courses = pagy(@ransack_courses.result.includes(:user))
+    render 'index'
   end
 
   def show
