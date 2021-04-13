ac36161d90bf092321b9d7f43dc1facae94453b8
banecikic <branimir.cikic@gmail.com>
2021-04-13 12:17:57 +0200

    add counter_cache to course enrollments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_06_111507) do
+ActiveRecord::Schema.define(version: 2021_04_13_094407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2021_04_06_111507) do
     t.string "level", default: "Beginner", null: false
     t.integer "price", default: 0, null: false
     t.float "average_rating", default: 0.0, null: false
+    t.integer "enrollments_count", default: 0, null: false
     t.index ["slug"], name: "index_courses_on_slug", unique: true
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
