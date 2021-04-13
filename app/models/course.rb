ee6c325a28dbb0efeea3fd9412c555d425bf50dd
banecikic <branimir.cikic@gmail.com>
2021-04-13 13:28:59 +0200

    add counter_cache accross application

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ApplicationRecord
   validates :title, :short_description, :language, :price, :level, presence:true
   validates :description, presence: true, length: { :minimum => 5}
 
-  belongs_to :user
+  belongs_to :user, counter_cache: true
   has_many :lessons, dependent: :destroy
   has_many :enrollments
 
